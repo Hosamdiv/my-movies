0f34c26ccb680055b6fb94bb84de929018e07228
hosam rda <rdhosam712@gmail.com>
2025-01-22 08:55:10 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { Provider as ProviderChakra } from "./components/ui/provider.tsx";
-// import { Provider as ProviderRedux } from "react-redux";
+import { Provider } from "./components/ui/provider.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
-// import { store } from "./App/store.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    {/* <ProviderRedux store={store}> */}
-      <ProviderChakra>
-        <App />
-      </ProviderChakra>
-    {/* </ProviderRedux> */}
+    <Provider>
+      <App />
+    </Provider>
   </QueryClientProvider>
 );
